39bd36167027a00077818236cfd141a86c2a1537
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-16 15:22:36 +0100

    stampa tramite api 10 email random

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        emails: [],
       }
+    },
+    mounted(){     
+        for (let i = 0; i <= 10; i++ )  {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const result = response.data;
+                this.emails.push(result.response);
+            });
+        }
     }
   }).mount('#app')
